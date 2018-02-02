a3212187948bf5fceba05b6b1c3fc31b83fcac3f
Leo的狂欢宇宙 <mayingnan@nannandemacbookyoudeMacBook-Pro.local>
2018-02-01 18:33:05 -0700

    lab4

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -1,14 +1,41 @@
+
+
+
+
+
+/*
+ *  Copyright © 2018 Team X, CMPUT301, University of Alberta - All Rights Reserved.
+ * You may use, distribute or modify this code under terms and conditions of Code of Student Behavior at
+ * University of Alberta.
+ * You can find a copy of the license in this project. Otherwise, please contact
+ *   contact@abc.ca
+ * /
+ */
+
 package ca.ualberta.cs.lonelytwitter;
 
 import java.util.Date;
 
 /**
- * Created by dezfuli on 1/16/18.
+ * Represents a tweet
+ *
+ * @author YingnanMa
+ *
+ * @version 1.0
+ *
+ * @see ImportantTweet
+ * @see NormalTweet
  */
 
 public abstract class Tweet implements Tweetable {
     private String message;
     private Date date;
+
+    /**
+     *Constructs a tweet instance using the given message
+     *
+     * @param message the message of the tweet
+     */
 
     Tweet(String message){
 
@@ -25,6 +52,16 @@ public abstract class Tweet implements Tweetable {
     public String getMessage(){
         return message;
     }
+
+    /**
+     * Sets the message of the tweet
+     *
+     * @param message - the new message of the tweet
+     * @throws TweetTooLongException - is thrown when the message is over 140 characters
+     *
+     * @see ImportantTweet
+     * @see NormalTweet
+     */
 
     public void setMessage(String message) throws TweetTooLongException{
         if (message.length() < 140){
